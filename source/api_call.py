2bfbc9984716637a17d23fc5a9b54d754a718aa1
detangvik <127206533+detangvik@users.noreply.github.com>
2023-05-12 11:48:58 +0000

    Lagt til eksempel chat-sekvens

--- source/api_call.py
+++ source/api_call.py
@@ -1,5 +1,5 @@
 import openai
-from source.api_keys import OPENAI_API_KEY
+from api_keys import OPENAI_API_KEY
 openai.api_key = OPENAI_API_KEY
 
 def generate_chatgpt_response(history):
@@ -7,6 +7,14 @@ def generate_chatgpt_response(history):
         model="gpt-3.5-turbo",
         max_tokens=500,
         temperature=1.2,
+        messages=history,
+    )
+    return response.choices[0].message.content
+
+def generate_chatgpt4_response(history):
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        temperature=0.5,
         messages=history,
     )
     return response.choices[0].message.content
